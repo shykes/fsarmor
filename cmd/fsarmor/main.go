3c3cba8fa2785e4d476c30bc04afc31d10c1b719
Solomon Hykes <solomon@docker.com>
2015-10-04 22:49:35 +0000

    Add a crude usage message for the test tool
    Signed-off-by: Solomon Hykes <solomon@docker.com>

--- cmd/fsarmor/main.go
+++ cmd/fsarmor/main.go
@@ -7,7 +7,15 @@ import (
 )
 
 func DieUsage() {
-	log.Fatalf("Usage: %s split|join DIR", os.Args[0])
+	log.Fatalf(`
+Usage: %s split|join DIR
+
+  %s split DIR: read a tar archive from STDIN and split it into an armored filesystem tree at DIR.
+                  DIR is created if needed.
+		  DIR is armored, which means it can be safely copied to any filesystem (including git) without information loss.
+
+  %s join DIR: write a tar archive to STDOUT from the contents of DIR, interpreted as an armored filesystem tree.
+`, os.Args[0], os.Args[0], os.Args[0])
 }
 
 func main() {
